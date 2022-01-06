0411a3ba8bd95ec28922f79f3d9bb223684479e3
David Luu <dluu.audeo@gmail.com>
2022-01-05 23:06:45 -0800

    created localstorage for scores

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,11 @@
 var secondsLeft = 60;
 var startBtn = document.querySelector(".start-button");
 var questions = document.querySelector(".questions");
+var scoresList = document.querySelector(".scores");
 var answers = document.querySelector(".answers");
 var timerEl = document.querySelector(".timer-count");
 var answerBtn = document.querySelectorAll(".answer-button");
+var submitBtn = document.querySelector("#submit");
 var isWin = false;
 var index = 0;
 var timerCount;
@@ -11,32 +13,40 @@ var timerCount;
 var questionList = [
     {
         question: "What basic language does web development utilize?",
-        options: ["CSS", "HTML", "JS"],
+        options: ["CSS", "HTML", "JS", "English"],
         answer: "HTML",
     }, 
     {
-        question: "How much coffee do you get in a tall Starbucks?",
-        options: ["12oz", "16oz", "24oz"],
-        answer: "12oz",
+        question: " The terms 'function' and 'var' are known as:",
+        options: ["Keywords", "Data types", "Declaration Statements", "Prototype"],
+        answer: "Declaration Statements",
     },
+    {
+        question: "What information can you store in a variable?",
+        options: ["Strings", "Numbers", "Boolean", "All of the Above"],
+        answer: "All of the Above",
+    },
+    {
+        question: "How do you create an array?",
+        options: ["var name = ('1,2,3')", "var name = ['1','2','3']", "var name = {'1', '2', '3'}"],
+        answer: "var name = ['1','2','3']",
+    },
+    {
+        question: "What are 'for' loops used for?",
+        options: ["To iterate over arrays", "To target a specific var", "To designate an 'html' element"],
+        answer: "To iterate over arrays",
+    },
+
 ];
 
 function startGame() {
     // isWin = false;
-    timerCount = 60;
+    timerCount = 100;
     startBtn.disabled = true;
     startTimer();
     displayQuestion();
 }
 
-function checkConditions(event) {
-    // IF user clicked an answer-btn
-    if (event.target.matches(".answer-button")) {
-        alert(event.target.dataset.answer === "1");
-        index += 1;
-        displayQuestion();
-    }
-}  
   
 function displayQuestion () {
     questions.innerHTML = "";
@@ -58,16 +68,11 @@ function displayQuestion () {
 
     for (var i = 0; i < questionCheck.options.length; i++) {    
         var optionText = questionCheck.options[i];
-        var btn = document.createElement("button");
-        btn.classList.add("answer-button");
-        btn.textContent = optionText;
+        var answerBtn = document.createElement("button");
+        answerBtn.classList.add("answer-button");
+        answerBtn.textContent = optionText;
 
-        if (optionText === questionCheck.answer) {
-            btn.dataset.answer = 1;
-        }   else {
-            btn.dataset.answers = 0;
-        }
-        optionsList.appendChild(btn);
+        optionsList.appendChild(answerBtn);
     }
 }
 
@@ -76,28 +81,59 @@ function startTimer() {
         timerCount--;
         timerEl.textContent = timerCount;
       if (timerCount >= 0) {
-        if (isWin && timerCount > 0) {
+        if (index >= questionList.length && timerCount > 0) {
            clearInterval(timer);
-        //   winGame();
+           var score = timerCount;
+           localStorage.setItem("scores", score);
+           endGame();
         }
       }
       
       if (timerCount <= 0) {
         clearInterval(timer);
-        // loseGame();
+        endGame();
       }
     }, 1000);
 } 
 
 function endGame() {
-    prompt("What are your first and last initials?");
+    questions.innerHTML = "";
+    document.getElementById("initialsInput").style.display = "unset";
 }
 
+function checkConditions(event) {
+    if (event.target.matches(".answer-button")) {
+        index += 1;
+        timerCount = timerCount - 10;
+    }
+
+    // if (answerBtn !== questionCheck.answer) {
+        // timerCount = timerCount - 10;
+    // }
+    
+    displayQuestion();
+}  
 
 startBtn.addEventListener("click", startGame);
-btn.addEventListener("click", checkConditions);
-answerBtn.addEventListener("click", checkConditions);
+questions.addEventListener("click", checkConditions);
 
-// localStorage.setItem("initials", scores);
+submitBtn.addEventListener("click", function(event) {
+    event.preventDefault();
 
-// input function for local storage
+    var initials = document.getElementById("initials").value;
+    localStorage.setItem("initials", initials);
+
+    var score = localStorage.getItem("scores");
+
+    var scoreLi = document.createElement("h4");
+    scoreLi.classList.add("scoreList");
+    scoreLi.textContent = (initials + " " + score);
+
+
+    scoresList.appendChild(scoreLi);
+
+    if (initials === "") {
+        alert("error", "initials cannot be blank");
+    } 
+
+});
